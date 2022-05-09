4d729cf7de6186de32bf2ea3e5029dc14b5abf70
Istvan Viczian <viczian.istvan@gmail.com>
2022-05-09 16:53:17 +0200

    Conversions

---
+++ conversion.py
@@ -0,0 +1,11 @@
+print(float(17))
+print(type(float(17)))
+
+print(int(17.2))
+print(int(17.8)) # Konvertálás NEM kerekít! Levágja a tizedesjegyeket.
+
+print(float("17.8"))
+
+print(float("17,8".replace(",", ".")))
+
+print("alma".upper())
