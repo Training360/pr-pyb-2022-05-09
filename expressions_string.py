49897335fdc903c98c9ead7cfaedd2da40f1eba1
Istvan Viczian <viczian.istvan@gmail.com>
2022-05-09 16:41:29 +0200

    Stringek és input

---
+++ expressions_string.py
@@ -0,0 +1,36 @@
+print("Hello")
+
+print("Hello" + "Word")
+
+name = "John Doe"
+
+print("Hello " + name)
+
+print(2 * name)
+
+index = 5
+print(type(index))
+
+print("Ez az " + str(index) + ". könyv.")
+print("Ez az", end="")
+print(index)
+
+print("Ez az ", index, ". könyv", sep="")
+# Ez az  5 . könyv
+
+print(f"Ez az {index} lemez.")
+
+print(f"Az 5 + 5 eredménye = {5 + 5}")
+
+print(1, 2, 3, 4, sep="-----")
+
+digit = "6"
+print(digit)
+digit += "x"
+print(digit)
+digit += "8"
+print(digit)
+digit += str(9)
+print(digit)
+
+
---
+++ input_demo.py
@@ -0,0 +1,19 @@
+# name = input("Mi a neved?")
+
+# print(name) # John Doe -> Hello John Doe!
+# print("Hello " + name + "!")
+
+# Mi a kedvenc számod?
+# 6
+# favourite_number
+# A kedvenc számod: 6
+# A kedvenc számod duplája: 12
+
+# answer = input("Mi a kedvenc számod?")
+# favourite_number = int(answer)
+
+favourite_number = int(input("Mi a kedvenc számod?"))
+
+print(type(favourite_number))
+print("Kedvenc számod: " + str(favourite_number))
+print("A kedvenc számod kétszerese: " + str(favourite_number * 2))
