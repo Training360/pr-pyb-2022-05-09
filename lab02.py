8dbe954fc767c281a3dc7ae368d01267f63eb7c3
Istvan Viczian <viczian.istvan@gmail.com>
2022-05-10 13:29:12 +0200

    elágazás

---
+++ lab02.py
@@ -0,0 +1,9 @@
+# Kérj be a felhasználótól egy egész számot! Majd amennyiben páros,
+# írd ki neki, hogy páros! Ha páratlan, írd ki neki, hogy páratlan!
+
+# Kérj be a felhasználótól egy másik egész számot! Amennyiben nagyobb mint,
+# nulla, írd ki, hogy pozitív! Ha nulla, írd ki, hogy nulla! Ellenkező esetben,
+# írd ki, hogy negatív!
+
+# Kérd be a felhasználó nevét! Ha nem írja be, akkor ír ki, hogy 
+# "ez nem volt szép!"! name == ""
