016760c5145073b0e3dfba24ce988241f4a73d9c
Istvan Viczian <viczian.istvan@gmail.com>
2022-05-10 11:26:04 +0200

    Feladatleírások - lab 01

---
+++ lab01.py
@@ -0,0 +1,14 @@
+# Írd ki egymás után 5x a neved!
+# Írd ki egymás utána a neved 5x, de írj elé sorszámot is. Pl.:
+# 0. Trainer
+# 1. Trainer
+# 2. Trainer
+# Írd ki egymás utána a neved 5x, írj elé sorszámot is, de egytől. 
+# Írd ki az első 10 pozitív egész számot, és mellé annak négyzetét!
+# 1 - 1
+# 2 - 4
+# 3 - 9
+# Hónapok! Írd ki az első 6 hónapra:
+# Az év 1. hónapja január.
+# Az év 2. hónapja február.
+# months = ["január", "február", "március"]
