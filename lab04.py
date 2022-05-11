64f2904dc8a1565d6dd2dcc5a6655581f69f3428
Istvan Viczian <viczian.istvan@gmail.com>
2022-05-11 13:03:36 +0200

    Feladatok 4

---
+++ lab04.py
@@ -0,0 +1,29 @@
+# Írjatok egy is_not_negative() nevű logikai függvényt, ami
+# True-t ad vissza, ha a szám nagyobb vagy egyenlő,
+# mint nulla!
+
+# Írjatok egy olyan get_circle_area() nevű függvényt,
+# melynek átadva a kör sugarát, visszaadja annak területét!
+# r^2*pi
+
+# Írjatok egy olyan get_rectangle_area() nevű függvényt,
+# ami visszaadja, a két oldalhossz alapján a téglalap területét!
+
+# Írjatok egy olyan absolute() nevű függvényt, ami visszaadja a 
+# paraméterként megadott szám abszolútértékét!
+
+# Írj egy sum_numbers() függvényt, ami paraméterül kap számok listáját,
+# és visszaadja a számok összegét!
+
+# Írj egy roll_dice() függvényt, ami visszaad egy véletlenszerű 1-6 közötti egész számot!
+
+# Írj egy count_positive_numbers() függvényt, ami paraméterül kap egész számok listáját,
+# és visszaadja, hogy hány pozitív szám van benne!
+
+
+def print_numbers(list):
+    for i in list:
+        print(i)
+
+numbers = [1, 2, 3, 4]
+print_numbers(numbers)
